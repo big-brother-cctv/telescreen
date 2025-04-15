18a611ce1698897a57ca4685759e0e9330b29ab8
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-15 17:25:54 +0200

    1st version

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9-slim
+
+RUN apt-get update && apt-get install -y \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender1 \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install -r requirements.txt --no-cache-dir
+
+EXPOSE 8501
+
+CMD ["streamlit", "run", "app.py"]
---
+++ app.py
@@ -0,0 +1,30 @@
+import cv2
+import streamlit as st
+import numpy as np
+
+RTSP_URL = "rtsp://mediamtx.local/stream"
+
+st.set_page_config(page_title="QR Code Generator", page_icon="🎥")
+
+st.title("Motion Detector")
+st.markdown("Visualiza el stream RTSP en esta página:")
+
+video_placeholder = st.empty()
+
+cap = cv2.VideoCapture(RTSP_URL)
+
+if not cap.isOpened():
+    st.error("Could not open stream.")
+else:
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            st.warning("Stream off.")
+            break
+        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        video_placeholder.image(frame_rgb, channels="RGB", use_column_width=True)
+
+cap.release()
