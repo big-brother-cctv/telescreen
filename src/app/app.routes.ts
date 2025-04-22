023fb412ed941282b8c72427d0f0d701eb1315af
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-22 21:25:14 +0200

    Add home, and proxy configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 import { CameraComponent } from './views/camera/camera.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'camera', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'camera', component: CameraComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' },
