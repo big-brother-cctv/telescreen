cc8981ee9c9b8b3bc9784fcd76247d64814d9e97
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-24 15:58:55 +0200

    Add multiple camera option view

--- src/app/views/camera/camera.component.ts
+++ src/app/views/camera/camera.component.ts
@@ -1,25 +1,48 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import Hls from 'hls.js';
 
 @Component({
   selector: 'app-camera',
   standalone: true,
   templateUrl: './camera.component.html',
-  imports: []
+  imports: [CommonModule]
 })
-export class CameraComponent implements AfterViewInit {
-  @ViewChild('video', { static: false }) videoRef!: ElementRef<HTMLVideoElement>;
+export class CameraComponent implements OnInit, AfterViewInit {
+  cameras: any[] = [];
+
+  @ViewChildren('video') videoRefs!: QueryList<ElementRef<HTMLVideoElement>>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('/api/cameras').subscribe(cameras => {
+      this.cameras = cameras || [];
+    });
+  }
 
   ngAfterViewInit(): void {
-    const video = this.videoRef?.nativeElement;
-    const streamUrl = '/stream/index.m3u8';
+    this.videoRefs.changes.subscribe(() => {
+      this.initStreams();
+    });
+    this.initStreams();
+  }
 
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(streamUrl);
-      hls.attachMedia(video);
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = streamUrl;
-    }
+  private initStreams() {
+    this.videoRefs.forEach((videoRef, idx) => {
+      const video = videoRef.nativeElement;
+      const camera = this.cameras[idx];
+      if (!camera) return;
+      const streamUrl = `/${camera.name}/index.m3u8`;
+
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(streamUrl);
+        hls.attachMedia(video);
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = streamUrl;
+      }
+    });
   }
 }
